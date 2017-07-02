e9bc06930b4eee866e3684163a52728ef1593066
József Kanczler <jozsef.kanczler@gmail.com>
2017-07-02 21:51:05 +0200

    Bug/update documentation (#44)
    * version is updated to 3.0.6
    
    * updated documentation and screenshots - part 1
    
    * version to 3.0.7
    
    * updated screenshots
    
    * 3.0.10
    
    * version to 3.0.11

--- version.ts
+++ version.ts
@@ -1,1 +1,1 @@
-export default "__GitVersion.SemVer__".replace("GitVersion.SemVer", "3.0.6").replace(/__/g, "");
+export default "__GitVersion.SemVer__".replace("GitVersion.SemVer", "3.0.11").replace(/__/g, "");
