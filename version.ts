03f28f0e12c8a4a15afe22c7daa3d27f26778bfa
József Kanczler <jozsef.kanczler@gmail.com>
2017-07-02 14:56:11 +0200

    version to 3.0.7

--- version.ts
+++ version.ts
@@ -1,1 +1,1 @@
-export default "__GitVersion.SemVer__".replace("GitVersion.SemVer", "3.0.6").replace(/__/g, "");
+export default "__GitVersion.SemVer__".replace("GitVersion.SemVer", "3.0.7").replace(/__/g, "");
