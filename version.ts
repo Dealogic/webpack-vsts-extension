38fb331455d17c20415f40e357e1304bf4ff6c82
József Kanczler <jozsef.kanczler@gmail.com>
2017-09-22 09:05:06 -0400

    GitVersion.yml is used as a source of version.ts

--- version.ts
+++ version.ts
@@ -1,1 +1,6 @@
-export default "__GitVersion.SemVer__".replace("GitVersion.SemVer", "3.2.0").replace(/__/g, "");
+import * as fs from "fs";
+import * as os from "os";
+
+const nextVersion = fs.readFileSync("./GitVersion.yml", "utf-8").split(os.EOL)[0].replace("next-version: ", "");
+
+export default "__GitVersion.SemVer__".replace("GitVersion.SemVer", nextVersion).replace(/__/g, "");
