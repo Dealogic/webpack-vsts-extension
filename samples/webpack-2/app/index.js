8609f7ca7dd9bee708e04f35d94aabbe85973491
József Kanczler <jozsef.kanczler@gmail.com>
2018-05-04 17:24:20 +0200

    dependencies are updated

--- samples/webpack-2/app/index.js
+++ samples/webpack-2/app/index.js
@@ -1,10 +1,7 @@
-import _ from 'lodash';
-
 function component () {
   var element = document.createElement('div');
 
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  element.innerHTML = 'Hello, webpack'
 
   return element;
 }
