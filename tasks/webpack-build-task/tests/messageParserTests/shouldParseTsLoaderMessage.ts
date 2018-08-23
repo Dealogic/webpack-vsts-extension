2fa64936137917dda4e3741591a871073c412432
József Kanczler <jozsef.kanczler@gmail.com>
2018-08-23 15:06:16 +0200

    unit tests are fixed

--- tasks/webpack-build-task/tests/messageParserTests/shouldParseTsLoaderMessage.ts
+++ tasks/webpack-build-task/tests/messageParserTests/shouldParseTsLoaderMessage.ts
@@ -6,7 +6,7 @@ export default () => {
     const unparsedTsLoaderMessage = `C:\\W\\Playground\\webpack-format-messages\\src\\index.ts\n./src/index.ts\n[tsl] ERROR in C:\\W\\Playground\\webpack-format-messages\\src\\index.ts(456,918)\r\n      TS2339: Property \'lo\' does not exist on type \'Console\'.`;
     const parsedMessage = parseTsLoaderMessage(unparsedTsLoaderMessage);
 
-    assert.equal(parsedMessage.filePath, "src/index.ts");
+    assert.equal(parsedMessage.filePath, "/src/index.ts");
     assert.equal(parsedMessage.message, unparsedTsLoaderMessage);
     assert.equal(parsedMessage.start.line, 456);
     assert.equal(parsedMessage.start.offset, 918);
