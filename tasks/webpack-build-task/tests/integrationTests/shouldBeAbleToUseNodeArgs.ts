2e800284f06f4459660c29399e3a06ebf382b8dc
Jozsef Kanczler <jozsef.kanczler@gmail.com>
2019-08-13 17:16:35 +0200

    remove unnecessary console.log

--- tasks/webpack-build-task/tests/integrationTests/shouldBeAbleToUseNodeArgs.ts
+++ tasks/webpack-build-task/tests/integrationTests/shouldBeAbleToUseNodeArgs.ts
@@ -12,10 +12,8 @@ export function executeTest(done: MochaDone): void {
         const testRunner = new MockTestRunner(testPath);
 
         testRunner.run();
-        console.log(testRunner.stdout);
 
         assert.isTrue(testRunner.succeeded, "webpack task should be succeeded");
-
         assert.include(testRunner.stdout, "executing the command: node --max_old_space_size=4096", "node args is not included");
 
         done();
